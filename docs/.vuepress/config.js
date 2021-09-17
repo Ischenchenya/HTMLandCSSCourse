cfc601edab229637134d94b571cd7158d1daeb15
YeungWanLum <295386487@qq.com>
2021-09-17 11:42:42 +0800

    :sparkles: 给HTML&CSS教程添加文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  base: "/HTMLandCSSCourse/",
+  lang: 'en-US',
+  title: "bilibli技术分享",
+  description: "哔哩哔哩分享的相关及文档",
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: `/images/icons/favicon-16x16.png`,
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: `/images/icons/favicon-32x32.png`,
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'application-name', content: 'VuePress' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'VuePress' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/images/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
+      },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  theme: '@vuepress/default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.svg',
+    searchMaxSuggestions: 10,
+    sidebarDepth: 3,
+    sidebar: 'auto',
+    navbar: require('./nav'),
+    sidebar: require('./sidebar')
+  },
+
+}
+
---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    title: 'HTML5',   // 必要的
+    path: '/HTML5/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+    sidebarDepth: 3,    // 可选的, 默认值是 1
+    children: [ /* ... */
+    ]
+  },
+  {
+    title: 'CSS3',
+    path: '/CSS3/',
+    sidebarDepth: 3,
+    children: [ /* ... */]
+  }
+]
